ed397b1243694678f1371494f5ccfe66f98d7d18
JB Duck-Mayr <j.duckmayr@gmail.com>
2019-02-27 15:10:38 -0600

    Add package documentation and unloading code

---
+++ R/gpmlr-package.R
@@ -0,0 +1,15 @@
+#' gpmlr
+#'
+#' Allows the GPML (Gaussian Processes for Machine Learning) Matlab algorithms
+#' to be called from R, using R syntax and data structures.
+#'
+#' @name gpmlr
+#' @docType package
+#' @author  JB Duck-Mayr
+#' @useDynLib gpmlr
+#' @importFrom Rcpp sourceCpp
+NULL
+.onUnload <- function (libpath) {
+    .exit_octave(FALSE)
+    library.dynam.unload('gpmlr', libpath)
+}
