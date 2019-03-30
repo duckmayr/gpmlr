ac5afec428d39fcc28ebfa77b160471d984a65f4
JB Duck-Mayr <j.duckmayr@gmail.com>
2019-03-30 06:47:57 -0500

    Support Octave 5.1.0

--- src/define-version.h
+++ src/define-version.h
@@ -3,14 +3,24 @@
 
 // We need to do different things for differenct Octave versions
 #include <octave/version.h>
-#if OCTAVE_MAJOR_VERSION >= 4 && OCTAVE_MINOR_VERSION >= 2
+
+// We temporarily make some abbreviations
+#define MAJOR OCTAVE_MAJOR_VERSION
+#define MINOR OCTAVE_MINOR_VERSION
+
+// Figure out whether the version is past certain cutpoints
+#if MAJOR > 4 || (MAJOR == 4 && MINOR >= 2)
     #define OCTAVE_4_2_OR_HIGHER
-    #if OCTAVE_MINOR_VERSION >= 4
+    #if MAJOR > 4 || (MAJOR == 4 && MINOR >= 4)
         #define OCTAVE_4_4_OR_HIGHER
     #endif // Octave version >= 4.4.0
 #else
     #define OCTAVE_BELOW_4_2
 #endif // Octave version >= 4.2.0
+
+// Undefine our abbreviations
+#undef MAJOR
+#undef MINOR
 
 #endif // OCTAVE_VERSION_DEFINED
 
